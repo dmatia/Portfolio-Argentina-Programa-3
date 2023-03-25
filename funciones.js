a33cd58c22e183d0e64f85005bbba3efd3fb5146
diego <83021753+dmatia@users.noreply.github.com>
2023-03-25 16:40:01 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,26 @@
+/*
+const main_content = document.getElementById('main_content');
+const boton = document.getElementById('boton');
+const texto = document.getElementById('aboutme_exp');
+
+boton.addEventListener('click', function() {
+    main_content.classList.toggle('main-content_exp');
+    boton.classList.toggle('titulo_exp');
+    texto.classList.toggle('content_exp');
+})
+
+*/
+
+var botones = document.querySelectorAll('.boton');
+const main_content = document.getElementById('main_content');
+const texto = document.querySelectorAll('.texto_exp');;
+
+botones.forEach((elemento, clave)=>{
+    elemento.addEventListener('click', () => {
+        main_content.classList.toggle('main-content_exp');
+        botones[clave].classList.toggle('titulo_exp');
+        texto[clave].classList.toggle('content_exp');
+
+
+    })
+})
